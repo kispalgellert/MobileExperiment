8df35c17c53524a6a3b7b53cae5a117cc5ecbea1
Ace <Ace@Aces-Mac.local>
2012-10-14 17:57:32 -0600

    Initial Commit

---
+++ Active/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Active
+//
+//  Created by Ace on 10/14/12.
+//  Copyright (c) 2012 Ace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ Active/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Active
+//
+//  Created by Ace on 10/14/12.
+//  Copyright (c) 2012 Ace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
